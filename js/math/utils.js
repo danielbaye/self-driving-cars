48a09da1714e99e02e992a1045be212e31ca9de1
danielbayevski <58001146+danielbayevski@users.noreply.github.com>
2023-12-16 22:26:28 +0200

    .

--- js/math/utils.js
+++ js/math/utils.js
@@ -23,7 +23,49 @@ function subtract(p1, p2) {
     return new Point(p1.x - p2.x, p1.y - p2.y);
 }
 
+function average(p1,p2){
+    return new Point((p1.x + p2.x)/2,(p1.y + p2.y)/2)
+
+}
 
 function scale(p, scaler) {
     return new Point(p.x * scaler, p.y * scaler)
+}
+
+function transate(loc, angle, offset) {
+    return new Point(
+        loc.x + Math.cos(angle) * offset,
+        loc.y + Math.sin(angle) * offset,
+    )
+}
+
+function getIntersection(a, b, c, d) {
+    const tTop = (d.x - c.x) * (a.y - c.y) - (d.y - c.y) * (a.x - c.x)
+    const uTop = (a.x - b.x) * (c.y - a.y) - (a.y - b.y) * (c.x - a.x)
+    const bottom = (b.x - a.x) * (d.y - c.y) - (b.y - a.y) * (d.x - c.x)
+
+    if (bottom != 0) {
+        const t = tTop / bottom;
+        const u = uTop / bottom;
+        if (t >= 0 && t <= 1 && u >= 0 && u <= 1)
+            return {
+                x: lerp(a.x, b.x, t),
+                y: lerp(a.y, b.y, t),
+                offset: t
+            }
+    }
+    return null;
+}
+
+function lerp(a, b, t) {
+    return a + (b - a) * t
+}
+
+function getRandomColor(){
+    const hue = 290 + Math.random()*260;
+    return "hsl("+hue+",100%,60%)"
+}
+
+function angle(point) {
+    return Math.atan2(point.y, point.x);
 }
--- js/primitives/envelope.js
+++ js/primitives/envelope.js
@@ -1,11 +1,30 @@
 class Envelope {
-    constructor(skeleton, width) {
+    constructor(skeleton, width, roundness = 9) {
         this.skeleton = skeleton;
-        this.poly = this.#generatepolygon(width)
+        this.poly = this.#generatepolygon(width, roundness)
     }
 
-    #generatepolygon(width) {
+    #generatepolygon(width, roundness) {
+        const { p1, p2 } = this.skeleton;
+        const radius = width / 2;
+        const alpha = angle(subtract(p1, p2))
+        const alpha_cw = alpha + Math.PI / 2;
+        const alpha_ccw = alpha - Math.PI / 2;
+        const points = [];
+        const step = Math.PI / Math.max(roundness, 1)
+        for (let i = alpha_ccw; i <= alpha_cw; i += step) {
+            points.push(transate(p1, i, radius))
 
+        }
+        for (let i = alpha_ccw; i <= alpha_cw; i += step) {
+            points.push(transate(p2, Math.PI + i, radius))
+
+        }
+        return new Polygon(points)
+    }
+
+    draw(ctx,options) {
+        this.poly.draw(ctx,options)
     }
 
 }
